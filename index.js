229d12edfa0a3aff98fd2d30728d0b9316862f01
Tsvetan Sirakov <72301625+kopieto@users.noreply.github.com>
2026-04-11 09:26:35 +0100

    add notitfication message on submit. update desiginer line

--- index.js
+++ index.js
@@ -12,47 +12,50 @@ navbarLinks.addEventListener("click", () => {
   }
 });
 
-
 // email js
 // 1. Initialize EmailJS with your Public Key
 
-window.onload = function() {
-    const form = document.getElementById('contact-form');
-    
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+window.onload = function () {
+  const form = document.getElementById("contact-form");
+  const notification = document.getElementById("notification");
 
-        // These IDs from your EmailJS dashboard
-        const serviceID = 'service_4qxahgm';
-        const templateID = 'template_pak5trb';
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                alert('Thank you for sending us a message. Will be contact you back shortly');
-                form.reset(); // Clear the form
-            }, (error) => {
-                alert('Failed to send: ' + JSON.stringify(error));
-            });
-    });
-}
+    // These IDs from your EmailJS dashboard
+    const serviceID = "service_4qxahgm";
+    const templateID = "template_pak5trb";
 
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        notification.classList.add("is-visible");
+        form.reset();
+        setTimeout(() => {
+          notification.classList.remove("is-visible");
+        }, 4000);
+      },
+      (error) => {
+        alert("Failed to send: " + JSON.stringify(error));
+      },
+    );
+  });
+};
 
 // carousel
-const buttons = document.querySelectorAll("[data-carousel-button]")
-
-buttons.forEach(button => {
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
     const slides = button
       .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+      .querySelector("[data-slides]");
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
